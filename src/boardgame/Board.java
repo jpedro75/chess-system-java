dc89fa1d69b1f655a88bb1d27c171081cc9cac51
João Pedro Martins Mazarin <joao.pedro.mazarim@gmail.com>
2021-04-29 18:41:32 -0300

    Camada Chess e imprimindo Board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -9,6 +9,24 @@ public Board(int rows, int columns) {
 	this.rows = rows;
 	this.columns = columns;
 	pieces = new Piece[rows][columns];
+	
 }
-
+public int getRows() {
+	return rows;
+}
+public void setRows(int rows) {
+	this.rows = rows;
+}
+public int getColumns() {
+	return columns;
+}
+public void setColumns(int columns) {
+	this.columns = columns;
+}
+public Piece piece(int row, int columns) {
+	return pieces[row][columns];
+}
+public Piece piece(Position position) {
+	return pieces[position.getRow()][position.getColumn()];
+}
 }
--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,15 +1,16 @@
 package boardgame;
 
 public class Piece {
-protected Position position;
-private Board board;
-public Piece(Board board) {
-	super();
-	this.board = board;
-}
-protected Board getBoard() {
-	return board;
-}
+	protected Position position;
+	private Board board;
+
+	public Piece(Board board) {
+		super();
+		this.board = board;
+	}
+
+	protected Board getBoard() {
+		return board;
+	}
 
 }
-
